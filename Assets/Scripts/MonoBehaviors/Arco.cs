27c7e67801bd545e3561e3f7092ab437c138da97
LucasFerrazNicolau <40740846+LucasFerrazNicolau@users.noreply.github.com>
2021-04-18 13:33:08 -0300

    Correção de direção da munição e da animação de atirando enquanto em movimento

--- Assets/Scripts/MonoBehaviors/Arco.cs
+++ Assets/Scripts/MonoBehaviors/Arco.cs
@@ -7,11 +7,18 @@ public class Arco : MonoBehaviour
     {
         var posicaoInicial = transform.position;
         var percentualCompleto = 0.0f;
+
+        // Rotação da munição
+        Vector3 direction = destino - posicaoInicial;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
         while (percentualCompleto < 1.0f)
         {
             percentualCompleto += Time.deltaTime / duracao;
-            var alturaCorrente = Mathf.Sin(Mathf.PI * percentualCompleto);
-            transform.position = Vector3.Lerp(posicaoInicial, destino, percentualCompleto) + Vector3.up * alturaCorrente;
+            //var alturaCorrente = Mathf.Sin(Mathf.PI * percentualCompleto);
+            //transform.position = Vector3.Lerp(posicaoInicial, destino, percentualCompleto) + Vector3.up * alturaCorrente;
+            transform.position = Vector3.Lerp(posicaoInicial, destino, percentualCompleto);
             percentualCompleto += Time.deltaTime / duracao;
             yield return null;
         }
