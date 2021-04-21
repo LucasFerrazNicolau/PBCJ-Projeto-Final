f4425b1509b5c2ad5a780496e52f427b73d475af
André B. Cardim <ab.cardim@hotmail.com>
2021-04-21 15:40:48 -0300

    Som no hit (Fase 1)

--- Assets/Scripts/MonoBehaviors/Municao.cs
+++ Assets/Scripts/MonoBehaviors/Municao.cs
@@ -3,10 +3,14 @@
 public class Municao : MonoBehaviour
 {
     public int danoCausado; // Poder de dano da munição
-    public AudioClip hit;
+    //public AudioClip hit;
+    public AudioSource enemyHitSource;
+    public GameObject audioSource;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        audioSource = GameObject.Find("Audio Source");
+        enemyHitSource = audioSource.GetComponent<AudioSource>();
         if (collision is BoxCollider2D)
         {
             if (collision.gameObject.CompareTag("Inimigo"))
@@ -14,6 +18,8 @@ public class Municao : MonoBehaviour
                 /*AudioSource hitprefab = GetComponent<AudioSource>();
                 hitprefab.clip = hit;
                 hitprefab.Play();*/
+                //Debug.Log("atingiu inimigo");
+                enemyHitSource.Play();
                 Inimigo inimigo = collision.gameObject.GetComponent<Inimigo>();
                 StartCoroutine(inimigo.DanoCaractere(danoCausado, 0.0f));
                 gameObject.SetActive(false);
