f1ed0219c6eaa33009ec256c136b996c66d92999
WesleyAxel <wesley.axel@hotmail.com>
2021-04-22 20:56:42 -0300

    Comentado os scripts wingamebutton e mudacena

--- Assets/Scripts/New_Scripts/MudaCena.cs
+++ Assets/Scripts/New_Scripts/MudaCena.cs
@@ -5,7 +5,7 @@ public class MudaCena : MonoBehaviour
 {
     public int indexCena = 0;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)         // metodo que verifica quando entra em colisão com player
     {
         if (collision.gameObject.tag == "Player")
         {
@@ -14,7 +14,7 @@ public class MudaCena : MonoBehaviour
     }
 
 
-    public void mudaCena()
+    public void mudaCena()                  //muda a cena com base no index
     {
         SceneManager.LoadScene(indexCena);
     }
